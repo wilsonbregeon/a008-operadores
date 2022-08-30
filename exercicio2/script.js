e1a69a6d5d3426a6402dbe3a6a6862b6f854399b
wilsonbregeon <wilsonbregeon@gmail.com>
2022-08-30 13:56:37 -0300

    exercicios-resolvidos

---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+let num1 = prompt("Digite um número!")
+let num2 = prompt("Digite outro número!")
+
+// Comparações
+comparacao1 = num1 > num2 
+console.log("O resultado é:" ,comparacao1)
+
+comparacao2 = num1 === num2
+console.log("O resultado é:" ,comparacao2)
+
+comparacao3 = num1 % num2
+console.log("O resultado é:" ,comparacao3)
