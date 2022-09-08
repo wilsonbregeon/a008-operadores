b8357a75496b3fe3dcf8b93751205c4db810796f
wilsonbregeon <wilsonbregeon@gmail.com>
2022-09-08 00:07:37 -0300

    exercicios-a008-operadores-resolvidos

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+const num = Number(prompt("Insira um número par!"))
+console.log(num % 2)
+
+//qualquer número par inserido terá como resultado final igual à 0
+//qualquer número ímpar inserido terá como resultado final igual à 1
---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+const num1 = Number(prompt("Digite um número!"))
+const num2 = Number(prompt("Digite outro número!"))
+
+console.log(num1 > num2)
+console.log(num1 < num2)
+console.log(num1 % num2 === 0)
+console.log(num2 % num1 === 0)
